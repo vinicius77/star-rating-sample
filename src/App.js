0a12c00acab1fcc132a0d2e8474558e1d7d324db
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-03-02 22:39:57 +0200

    feat(star-rating): Star rating working properly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import StarRating from './StarRating.jsx';
-import RatingStar from './RatingStar.jsx';
+import StarRating from './StarRating';
+import RatingStar from './RatingStar';
+import BestRating from './BestRating/BestRating';
 
 function App() {
 	return (
 		<div>
 			<StarRating />
 			<RatingStar stars="3.5" />
+			<BestRating />
 		</div>
 	);
 }
