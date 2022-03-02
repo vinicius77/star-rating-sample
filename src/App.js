e36e58107be0d390ca8d60144319e7adca9eb27c
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-03-02 22:23:16 +0200

    Just importing Stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import StarRating from './StarRating.jsx';
+import RatingStar from './RatingStar.jsx';
 
 function App() {
 	return (
 		<div>
 			<StarRating />
+			<RatingStar stars="3.5" />
 		</div>
 	);
 }
